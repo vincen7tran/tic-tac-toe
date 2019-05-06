c3d7a1d345814a34be2b0723f132c50ec0b64104
Vinh Tran <vincen7.tran@gmail.com>
2019-05-06 11:56:43 -0700

    endGame and checkVictory functioning

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
 const spaces = document.querySelectorAll('.space');
 spaces.forEach(space => space.addEventListener('click', handleClick));
 
-let turn = 0;
-const xBoard = [];
-const oBoard = [];
+let playerXTurn = true;
+const board = {};
 const totalSpaces = 9;
+const blankPlayerBoard = {
+  rowOne: 0, rowTwo: 0, rowThree: 0,
+  majorDiag: 0, minorDiag: 0,
+  colOne: 0, colTwo: 0, colThree: 0
+};
+const xBoard = Object.assign({}, blankPlayerBoard)
+const oBoard = Object.assign({}, blankPlayerBoard);
 
 function handleClick() {
-  if (this.textContent) alert('Space is already taken!');
+  const id = parseInt(this.getAttribute('id'));
+  if (board[id]) alert('Space is already taken!');
   else {
-    if (turn % 2 === 0) this.textContent = 'X';
+    if (playerXTurn) this.textContent = 'X';
     else this.textContent  = 'O';
-    setBoard(this, turn);
-    turn++;
+    setBoard(playerXTurn, id);
+    playerXTurn = !playerXTurn;
   }
 }
 
-function setBoard(space) {
-  const id = parseInt(space.getAttribute('id'));
-  if (turn % 2 === 0) xBoard.push(id);
-  else oBoard.push(id);
+function setBoard(playerXTurn, id) {
+  
+  board[id] = true;
+  const currentBoard = playerXTurn ? xBoard : oBoard;
+
+  if (id === 0) {
+    currentBoard['rowOne']++;
+    currentBoard['colOne']++;
+    currentBoard['majorDiag']++;
+  } else if (id === 1) {
+    currentBoard['rowOne']++;
+    currentBoard['colTwo']++;
+  } else if (id === 2) {
+    currentBoard['rowOne']++;
+    currentBoard['colThree']++;
+    currentBoard['minorDiag']++;
+  } else if (id === 3) {
+    currentBoard['rowTwo']++;
+    currentBoard['colOne']++;
+  } else if (id === 4) {
+    currentBoard['rowTwo']++;
+    currentBoard['colTwo']++;
+    currentBoard['majorDiag']++;
+    currentBoard['minorDiag']++;
+  } else if (id === 5) {
+    currentBoard['rowTwo']++;
+    currentBoard['colThree']++;
+  } else if (id === 6) {
+    currentBoard['rowThree']++;
+    currentBoard['colOne']++;
+    currentBoard['minorDiag']++;
+  } else if (id === 7) {
+    currentBoard['rowThree']++;
+    currentBoard['colTwo']++;
+  } else if (id === 8) {
+    currentBoard['rowThree']++;
+    currentBoard['colThree']++;
+    currentBoard['majorDiag']++;
+  }
+  checkVictory();
 }
 
-function checkRows() {
-
+function checkVictory() {
+  for (let key in xBoard) {
+    if (xBoard[key] === 3) endGame('X');
+  }
+  for (let key in oBoard) {
+    if (oBoard[key] === 3) endGame('O');
+  }
+  if (Object.keys(board).length === totalSpaces) endGame('draw')
 }
 
-function checkColumns() {
-
-}
-
-function checkDiagonals() {
-
+function endGame(status) {
+  if (status === 'X') alert('Player X Wins!');
+  else if (status === 'O') alert('Player O Wins!');
+  else alert('DRAW!')
 }
